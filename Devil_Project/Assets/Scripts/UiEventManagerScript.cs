be75a40e430e6b7bebca134497ac23c209a505df
DanteHalfeye <deadcoolest@gmail.com>
2024-01-26 21:30:19 -0500

    SimpleDialogueSystem

---
+++ Devil_Project/Assets/Scripts/UiEventManagerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiEventManagerScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    public void CloseDialogue()
+    {
+        print("dialogue");
+    }
+
+    public void Machetear() 
+    {
+        print("Machetear");
+    }
+}
