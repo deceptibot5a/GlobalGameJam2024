635df1513daadf2638c379cc84a5738a3b1f81c9
DanteHalfeye <deadcoolest@gmail.com>
2024-01-28 13:15:25 -0500

    .

--- Devil_Project/Assets/Scripts/CamarasNivel2.cs
+++ Devil_Project/Assets/Scripts/CamarasNivel2.cs
@@ -5,71 +5,70 @@ using UnityEngine;
 
 public class CamarasNivel2 : MonoBehaviour
 {
-    // #region Fields
+    //// #region Fields
 
-    [SerializeField] private CinemachineVirtualCamera virtualCamera1;
-    [SerializeField] private CinemachineVirtualCamera virtualCamera2;
-    [SerializeField] private CinemachineVirtualCamera virtualCamera3;
-    [SerializeField] private CinemachineVirtualCamera virtualCamera4;
-
-
-    // #endregion
-
-    private void Start()
-    {
-        virtualCamera1.Priority = 15;
-        virtualCamera2.Priority = 0;
-        virtualCamera3.Priority = 0;
-        virtualCamera4.Priority = 0;
-
-    }
+    //[SerializeField] private CinemachineVirtualCamera virtualCamera1;
+    //[SerializeField] private CinemachineVirtualCamera virtualCamera2;
+    //[SerializeField] private CinemachineVirtualCamera virtualCamera3;
+    //[SerializeField] private CinemachineVirtualCamera virtualCamera4;
 
 
+    //// #endregion
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        collision.TryGetComponent<teleport1>(out teleport1 item);
-        if (item)
-        {
+    //private void Start()
+    //{
+    //    virtualCamera1.Priority = 15;
+    //    virtualCamera2.Priority = 0;
+    //    virtualCamera3.Priority = 0;
+    //    virtualCamera4.Priority = 0;
 
-            virtualCamera2.Priority = virtualCamera1.Priority + 1;
-            virtualCamera1.Priority = 0;
+    //}
 
-        }
-        //intento
 
-        if (item.CompareTag("habitacion1"))
-        {
-            virtualCamera1.Priority = 15;
-            virtualCamera2.Priority = 0;
-            virtualCamera3.Priority = 0;
-            virtualCamera4.Priority = 0;
-        }
 
-        if (item.CompareTag("habitacion2"))
-        {
-            virtualCamera1.Priority = 0;
-            virtualCamera2.Priority = 15;
-            virtualCamera3.Priority = 0;
-            virtualCamera4.Priority = 0;
-        }
-        //aca sirve
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    collision.TryGetComponent<teleport1>(out teleport1 item);
+    //    if (item)
+    //    {
 
-        if (item.CompareTag("habitacion3"))
-        {
-            virtualCamera1.Priority = 0;
-            virtualCamera2.Priority = 0;
-            virtualCamera3.Priority = 15;
-            virtualCamera4.Priority = 0;
-        }
+    //        virtualCamera2.Priority = virtualCamera1.Priority + 1;
+    //        virtualCamera1.Priority = 0;
 
-        if (item.CompareTag("habitacion4"))
-        {
-            virtualCamera1.Priority = 0;
-            virtualCamera2.Priority = 0;
-            virtualCamera3.Priority = 0;
-            virtualCamera4.Priority = 15;
-        }
-        //intento
-    }
+    //    }
+    //    //intento
+
+    //    if (item.CompareTag("habitacion1") == true )
+    //    {
+    //        virtualCamera1.Priority = 15;
+    //        virtualCamera2.Priority = 0;
+    //        virtualCamera3.Priority = 0;
+    //        virtualCamera4.Priority = 0;
+    //    }
+    //    else if (item.CompareTag("habitacion2") == true)
+    //    {
+    //        virtualCamera1.Priority = 0;
+    //        virtualCamera2.Priority = 15;
+    //        virtualCamera3.Priority = 0;
+    //        virtualCamera4.Priority = 0;
+    //    }
+    //    //aca sirve
+
+    //    else if (item.CompareTag("habitacion3"))
+    //    {
+    //        virtualCamera1.Priority = 0;
+    //        virtualCamera2.Priority = 0;
+    //        virtualCamera3.Priority = 15;
+    //        virtualCamera4.Priority = 0;
+    //    }
+
+    //    else if (item.CompareTag("habitacion4"))
+    //    {
+    //        virtualCamera1.Priority = 0;
+    //        virtualCamera2.Priority = 0;
+    //        virtualCamera3.Priority = 0;
+    //        virtualCamera4.Priority = 15;
+    //    }
+    //    //intento
+    //}
 }
