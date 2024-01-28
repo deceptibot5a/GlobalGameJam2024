9e95f3866d8049acb5002801cd028007d8ccd99c
IssaMs46 <isams2004.1@gmail.com>
2024-01-28 14:01:59 -0500

    YA SIRVE AAAAA

--- Devil_Project/Assets/Scripts/teleport1.cs
+++ Devil_Project/Assets/Scripts/teleport1.cs
@@ -45,20 +45,27 @@ using UnityEngine;
 
 public class teleport1 : MonoBehaviour
 {
+    Camaras camaras;
     public GameObject portal1; // [SerializeField]
-    private GameObject player;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        player = GameObject.FindWithTag("Player");
+       
+       camaras = FindObjectOfType<Camaras>();
     }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
-        { 
-            player.transform.position = new Vector2(portal1.transform.position.x, portal1.transform.position.y);
+        Collider2D collider = this.GetComponent<Collider2D>();
+        if (collision.tag == "NPC")
+        {
+            collision.transform.position = new Vector2(portal1.transform.position.x, portal1.transform.position.y);
+        }
+        if (collision.tag == "Player" )
+        {
+            collision.transform.position = new Vector2(portal1.transform.position.x, portal1.transform.position.y);
+            camaras.DetectCollision(collider);
         }
     }
 }
