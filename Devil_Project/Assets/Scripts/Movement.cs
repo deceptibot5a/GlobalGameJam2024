8c2ca04d1fec97b5f163e91eaf2a30a0f0e38042
DanteHalfeye <deadcoolest@gmail.com>
2024-01-28 10:59:46 -0500

    .

--- Devil_Project/Assets/Scripts/Movement.cs
+++ Devil_Project/Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,16 @@ public class Movement : MonoBehaviour
 {
 
     [SerializeField] private float speed = 3f;
+    SpriteRenderer spriteRenderer;
 
+    bool facingRight = true;
     private Rigidbody2D player;
 
     private Vector2 moveInput;
 
-    void Start()
+    void Awake()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         player = GetComponent<Rigidbody2D>();
     }
 
@@ -23,9 +27,21 @@ public class Movement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
         moveInput = new Vector2(moveX, moveY).normalized;
+        if (moveX < 0 && facingRight)
+        {
+            Flip();
+        }
+        else if (moveX > 0 && !facingRight)
+        {
+            Flip();
+        }
     }
 
-    
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
 
     private void FixedUpdate()
     {
